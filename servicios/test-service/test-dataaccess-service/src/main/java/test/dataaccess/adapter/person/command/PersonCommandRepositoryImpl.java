1b09a41e82b541715e70f639bb953c140da2507b
Carlos.Tontaquimba <carlos.tontaquimba@funcionjudicial.gob.ec>
2024-08-12 08:54:33 -0500

    fix: Message for create person modified

--- servicios/test-service/test-dataaccess-service/src/main/java/test/dataaccess/adapter/person/command/PersonCommandRepositoryImpl.java
+++ servicios/test-service/test-dataaccess-service/src/main/java/test/dataaccess/adapter/person/command/PersonCommandRepositoryImpl.java
@@ -1,5 +1,7 @@
 package test.dataaccess.adapter.person.command;
 
+import java.util.Optional;
+import java.util.concurrent.locks.StampedLock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,91 +16,88 @@ import test.test.record.ExceptionResponseRecord;
 import test.test.record.request.PersonRequestRecord;
 import test.test.record.response.PersonResponseRecord;
 
-import java.util.Optional;
-import java.util.concurrent.locks.StampedLock;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PersonCommandRepositoryImpl implements PersonCommandRepository {
 
-    private final PersonJpaRepository personJpaRepository;
+  private final PersonJpaRepository personJpaRepository;
 
-    @Override
-    @Transactional
-    public ResponseEntity<ExceptionResponseRecord> createOrUpdate(
-            PersonRequestRecord personRequestRecord) {
+  @Override
+  @Transactional
+  public ResponseEntity<ExceptionResponseRecord> createOrUpdate(
+      PersonRequestRecord personRequestRecord) {
 
-        PersonEntity entity = getPersonEntity(personRequestRecord);
-        PersonResponseRecord saved = PersonMapper.INSTANCE.entityToResponseRecord(
-                personJpaRepository.save(entity));
-        return createSuccessResponse(
-                "Cuenta almacenada para el usuario: ",
-                saved);
+    PersonEntity entity = getPersonEntity(personRequestRecord);
+    PersonResponseRecord saved = PersonMapper.INSTANCE.entityToResponseRecord(
+        personJpaRepository.save(entity));
+    return createSuccessResponse(
+        "Registrado !",
+        saved);
+  }
+
+  @Override
+  @Transactional
+  public ResponseEntity<ExceptionResponseRecord> delete(String id) {
+    return personJpaRepository.findByIdNumber(id)
+        .map(this::deletePerson)
+        .orElseGet(() -> createErrorResponse("No se encontró ningún registro"));
+  }
+
+  private ResponseEntity<ExceptionResponseRecord> deletePerson(PersonEntity person) {
+    personJpaRepository.deletePersonEntityByIdNumber(person.getIdNumber());
+    return createSuccessResponse("Persona eliminada",
+        PersonMapper.INSTANCE.entityToResponseRecord(person));
+  }
+
+  private PersonEntity updateExistingPerson(PersonRequestRecord requestRecord) {
+    PersonEntity existingEntity =
+        personJpaRepository
+            .findById(requestRecord.id())
+            .orElseThrow(() -> new CreateException("Persona no encontrada"));
+
+    final StampedLock lock = existingEntity.getLock();
+    long stamp = lock.writeLock();
+    try {
+      existingEntity.setName(requestRecord.name());
+      existingEntity.setGender(requestRecord.gender());
+      existingEntity.setStatus(requestRecord.status());
+      existingEntity.setAge(requestRecord.age());
+      existingEntity.setIdNumber(requestRecord.idNumber());
+      existingEntity.setAddress(requestRecord.address());
+      existingEntity.setPhone(requestRecord.phone());
+
+    } finally {
+      lock.unlockWrite(stamp);
     }
+    return existingEntity;
+  }
 
-    @Override
-    @Transactional
-    public ResponseEntity<ExceptionResponseRecord> delete(String id) {
-        return personJpaRepository.findByIdNumber(id)
-                .map(this::deletePerson)
-                .orElseGet(() -> createErrorResponse("No se encontró ningún registro"));
-    }
+  private PersonEntity getPersonEntity(PersonRequestRecord personRequestRecord) {
+    Optional<PersonEntity> personOptional = personJpaRepository.findByIdNumber(
+        personRequestRecord.idNumber());
+    return personOptional.isPresent() ? updateExistingPerson(personRequestRecord)
+        : createNewPerson(personRequestRecord);
+  }
 
-    private ResponseEntity<ExceptionResponseRecord> deletePerson(PersonEntity person) {
-        personJpaRepository.deletePersonEntityByIdNumber(person.getIdNumber());
-        return createSuccessResponse("Persona eliminada",
-                PersonMapper.INSTANCE.entityToResponseRecord(person));
-    }
+  private PersonEntity createNewPerson(PersonRequestRecord personRequestRecord) {
+    return PersonMapper.INSTANCE.requestRecordToEntity(personRequestRecord);
+  }
 
-    private PersonEntity updateExistingPerson(PersonRequestRecord requestRecord) {
-        PersonEntity existingEntity =
-                personJpaRepository
-                        .findById(requestRecord.id())
-                        .orElseThrow(() -> new CreateException("Persona no encontrada"));
+  private ResponseEntity<ExceptionResponseRecord> createSuccessResponse(String message,
+      PersonResponseRecord saved) {
+    return new ResponseEntity<>(CreateException(message, saved), HttpStatus.OK);
+  }
 
-        final StampedLock lock = existingEntity.getLock();
-        long stamp = lock.writeLock();
-        try {
-            existingEntity.setName(requestRecord.name());
-            existingEntity.setGender(requestRecord.gender());
-            existingEntity.setStatus(requestRecord.status());
-            existingEntity.setAge(requestRecord.age());
-            existingEntity.setIdNumber(requestRecord.idNumber());
-            existingEntity.setAddress(requestRecord.address());
-            existingEntity.setPhone(requestRecord.phone());
+  private ExceptionResponseRecord CreateException(String message, Object o) {
+    return ExceptionResponseRecord.builder()
+        .httpStatus(HttpStatus.ACCEPTED)
+        .message(message)
+        .data(o)
+        .build();
+  }
 
-        } finally {
-            lock.unlockWrite(stamp);
-        }
-        return existingEntity;
-    }
-
-    private PersonEntity getPersonEntity(PersonRequestRecord personRequestRecord) {
-        Optional<PersonEntity> personOptional = personJpaRepository.findByIdNumber(
-                personRequestRecord.idNumber());
-        return personOptional.isPresent() ? updateExistingPerson(personRequestRecord)
-                : createNewPerson(personRequestRecord);
-    }
-
-    private PersonEntity createNewPerson(PersonRequestRecord personRequestRecord) {
-        return PersonMapper.INSTANCE.requestRecordToEntity(personRequestRecord);
-    }
-
-    private ResponseEntity<ExceptionResponseRecord> createSuccessResponse(String message,
-                                                                          PersonResponseRecord saved) {
-        return new ResponseEntity<>(CreateException(message, saved), HttpStatus.OK);
-    }
-
-    private ExceptionResponseRecord CreateException(String message, Object o) {
-        return ExceptionResponseRecord.builder()
-                .httpStatus(HttpStatus.ACCEPTED)
-                .message(message)
-                .data(o)
-                .build();
-    }
-
-    private ResponseEntity<ExceptionResponseRecord> createErrorResponse(String message) {
-        return new ResponseEntity<>(CreateException(message, null), HttpStatus.OK);
-    }
+  private ResponseEntity<ExceptionResponseRecord> createErrorResponse(String message) {
+    return new ResponseEntity<>(CreateException(message, null), HttpStatus.OK);
+  }
 }
